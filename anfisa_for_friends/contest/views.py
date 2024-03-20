a7451840029f2577622c76d271512a219305f4b9
Lev Akchurin <levisserena@yandex.ru>
2024-03-20 14:18:16 +0400

    Научил записывать конкурсантов в БД, и появилась страница со списком конкурсантов.

--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
 from .forms import ContestForm
+from .models import Contest
 
 
 def proposal(request):
     form = ContestForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
-        pass
+        form.save()
     return render(request, 'contest/form.html', context)
+
+
+def proposal_list(request):
+    ice_creams = Contest.objects.all().order_by('id')
+    context = { 'ice_creams': ice_creams}
+    return render(request, 'contest/contest_list.html', context)
