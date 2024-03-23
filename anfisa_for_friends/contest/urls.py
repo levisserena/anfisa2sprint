f87711ca85b38283eb692f70f16c7292c98025ec
Lev Akchurin <levisserena@yandex.ru>
2024-03-23 14:58:21 +0400

    Теперь пользователь может изменять и удалять записи в БД

--- anfisa_for_friends/contest/urls.py
+++ anfisa_for_friends/contest/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'contest'
 
 urlpatterns = [
-    path('', views.proposal, name='create'),
     path('list/', views.proposal_list, name='list'),
+    path('<int:pk>/edit/', views.proposal, name='edit'),
+    path('<int:pk>/delete/', views.delete_proposal, name='delete'),
+    path('', views.proposal, name='create'),
 ]
--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import ContestForm
 from .models import Contest
 
 
-def proposal(request):
-    form = ContestForm(request.POST or None)
+def proposal(request, pk=None):
+    if pk is None:
+        instance = None
+    else:
+        instance = get_object_or_404(Contest, pk=pk)
+    form = ContestForm(request.POST or None, instance=instance)
     context = {'form': form}
     if form.is_valid():
         form.save()
@@ -14,5 +18,15 @@ def proposal(request):
 
 def proposal_list(request):
     ice_creams = Contest.objects.all().order_by('id')
-    context = { 'ice_creams': ice_creams}
+    context = {'ice_creams': ice_creams}
     return render(request, 'contest/contest_list.html', context)
+
+
+def delete_proposal(request, pk):
+    instance = get_object_or_404(Contest, pk=pk)
+    form = ContestForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('contest:list')
+    return render(request, 'contest/form.html', context)
