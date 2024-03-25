8d331182b5e5e1642dee7d2802a05485bea1b0e1
Lev Akchurin <levisserena@yandex.ru>
2024-03-25 12:55:26 +0400

    Изменил страничку about: теперь используютсы CBV.

--- anfisa_for_friends/about/views.py
+++ anfisa_for_friends/about/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
+from contest.models import Contest
 
-def description(request):
-    template = 'about/description.html'
-    return render(request, template)
+class Description(TemplateView):
+    template_name = 'about/description.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_count'] = Contest.objects.count()
+        return context
